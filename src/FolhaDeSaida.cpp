7a79e73aeaa8ed93a568c8585c74cf52e0c74ac9
WilkerSebastian <wilkerafonso2005@gmail.com>
2023-03-26 17:05:13 -0400

    iniando playground

--- src/FolhaDeSaida.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include "../include/FolhaDeSaida.hpp"
-#include <iostream>
-
-void FolhaDeSaida::imprimir(std::string texto) {
-
-    std::cout << "\nfolha de saida: " << texto << "\n";
-
-}
