fc43d6d474f5ca20c081b1af118ec0c87bc5605d
WilkerSebastian <wilkerafonso2005@gmail.com>
2024-01-16 11:45:08 -0400

    starting over

--- src/FolhaDeSaida.cpp
+++
@@ -1,9 +0,0 @@
-#include "FolhaDeSaida.hpp"
-#include <string>
-#include <iostream>
-
-void FolhaDeSaida::imprimir(std::string texto) {
-
-    std::cout << "\nfolha de saida: " << texto << "\n";
-
-}
